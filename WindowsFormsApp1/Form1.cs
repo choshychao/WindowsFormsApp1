cecf88c864641cab0a128713dba1661e036931a6
chosuar16 <chosuar16@gmail.com>
2019-01-11 16:26:58 +0800

    Added Message Box

--- WindowsFormsApp1/Form1.cs
+++ WindowsFormsApp1/Form1.cs
@@ -25,6 +25,7 @@ namespace WindowsFormsApp1
         private void button2_Click(object sender, EventArgs e)
         {
             staff1.BringToFront();
+            MessageBox.Show("Hello");
         }
     }
 }
